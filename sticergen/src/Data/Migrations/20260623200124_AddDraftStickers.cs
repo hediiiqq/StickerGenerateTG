2aeea0f7b11e09b7112f4f2f7f50a9869460fb2b
whymiutu31 <mgus7900@gmail.com>
2026-06-24 20:51:08 +0300

    add addDraftstickers migrate

---
+++ sticergen/src/Data/Migrations/20260623200124_AddDraftStickers.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace sticergen.src.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDraftStickers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DraftStickers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DraftId = table.Column<int>(type: "integer", nullable: false),
+                    TelegramFileId = table.Column<string>(type: "text", nullable: false),
+                    Emoji = table.Column<string>(type: "text", nullable: false),
+                    SortOrder = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DraftStickers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DraftStickers_Drafts_DraftId",
+                        column: x => x.DraftId,
+                        principalTable: "Drafts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DraftStickers_DraftId",
+                table: "DraftStickers",
+                column: "DraftId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DraftStickers");
+        }
+    }
+}
